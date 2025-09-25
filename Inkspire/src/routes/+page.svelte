a8477fe5762a79c9d7a8f3499b2b28f6f38eb844
enithebest <aenihasmegaj@gmail.com>
2025-09-25 21:51:43 +0100

    shadcn components

--- Inkspire/src/routes/+page.svelte
+++ Inkspire/src/routes/+page.svelte
@@ -4,3 +4,4 @@
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
 <h2>Welcome to SvelteKit</h2>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
