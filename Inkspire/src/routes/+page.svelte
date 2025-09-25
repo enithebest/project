265156f679a810e98b0f06e7f8756b62a9e1d334
enithebest <aenihasmegaj@gmail.com>
2025-09-25 21:04:36 +0100

    comit kot sa per qef

--- Inkspire/src/routes/+page.svelte
+++ Inkspire/src/routes/+page.svelte
@@ -1,2 +1,6 @@
-<h1>Welcome to SvelteKit</h1>
+<h2 class="bg-blue-600">Welcome to SvelteKit</h2>
+<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<h2>Welcome to SvelteKit</h2>
+<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<h2>Welcome to SvelteKit</h2>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
